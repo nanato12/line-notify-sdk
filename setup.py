7cb073dc28e9a4e734fcf6e5162bd1a2be524034
nanato12 <admin@okj.info>
2024-05-28 19:28:44 +0900

    update: setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup
 
-setup()
+import notify
+
+setup(
+    license=notify.__license__,
+    description=notify.__description__,
+    author=notify.__author__,
+    author_email=notify.__author_email__,
+    maintainer=notify.__author__,
+    maintainer_email=notify.__author_email__,
+    url=notify.__url__,
+)
